b67dfa9d947d4e0a468b53c93d4434311b2eecd9
EclidFundgue <1011379214@qq.com>
2024-09-26 19:22:35 +0800

    add yolo model config

--- src/process/correct.py
+++ src/process/correct.py
@@ -1,16 +1,23 @@
+import os
 from typing import List
 
 import cv2
 
+from ..resources_loader import ConfigLoader
+from ..utils.constants import ROOT_PATH
 from .label_io import LabelInputOutput
 from .verify_points import correctLabelByPoints
 
-# DOTO: import Yolov8Net
-# from .yolov8 import Yolov8Net
-# net = Yolov8Net()
+# Load network by config
+__loader = ConfigLoader()
+if __loader['load_network']:
+    from .yolov8 import Yolov8Net
+    net = Yolov8Net(os.path.join(ROOT_PATH, __loader['model_path']))
+
 
 def relabel(img: cv2.Mat, original_labels: List[LabelInputOutput]) -> List[LabelInputOutput]:
-    # return net(img)
+    if __loader['load_network']:
+        return net(img)
     return original_labels
 
 def correctLabels(img: cv2.Mat, labels: List[LabelInputOutput]) -> List[LabelInputOutput]:
--- src/process/yolov8/inference.py
+++ src/process/yolov8/inference.py
@@ -4,19 +4,17 @@ from typing import List
 import cv2
 from ultralytics import YOLO
 
-from ...utils.constants import ROOT_PATH
 from ..label_io import LabelInputOutput
 from .translator import predict_with_split_windows
 
 
 class Yolov8Net:
-    def __init__(self):
-        model_path = os.path.join(ROOT_PATH, 'resources/yolov8x_640.onnx')
-        self.model = YOLO(model_path, task='pose')
+    def __init__(self, path: str):
+        self.model = YOLO(path, task='pose')
 
     def __call__(self, img: cv2.Mat) -> List[LabelInputOutput]:
         h, w, ch = img.shape
-        ls = predict_with_split_windows(self.model, img, 720)
+        ls = predict_with_split_windows(self.model, img, 544)
         res = []
         for lb in ls:
             idx = lb[0]
