510db588cbbefb883a07eb1ef4ee11df28e41f9b
EclidFundgue <1011379214@qq.com>
2024-10-11 13:54:43 +0800

    add TextButton component

--- src/pygame_gui/components/button.py
+++ src/pygame_gui/components/button.py
@@ -1,15 +1,16 @@
-from typing import Callable, Union
+from typing import Callable, Tuple, Union
 
 import pygame
 from pygame import Surface as pg_Surface
 
 from ..decorators import getCallable
-from .surface import Surface
+from ..smooth import SmoothColor
+from .surface import BaseComponent
 
 
-class Button(Surface):
+class Button(BaseComponent):
     '''
-    Click, then call `on_press` funcion.
+    A button triggers a callback function when clicked on.
 
     Button(
         w, h, x, y,
@@ -21,8 +22,7 @@ class Button(Surface):
     )
     '''
     def __init__(self,
-            w: int, h: int,
-            x: int, y: int,
+            w: int, h: int, x: int, y: int,
             image: Union[str, pg_Surface],
             pressed_image: Union[str, pg_Surface] = None,
             on_press: Callable = None,
@@ -81,3 +81,65 @@ class Button(Surface):
             surface.blit(self.pressed_image, (self.x, self.y))
         else:
             surface.blit(self.image, (self.x, self.y))
+
+class TextButton(Button):
+    '''
+    A button with text label.
+    '''
+
+    DEFAULT_FONT = None
+
+    def __init__(self,
+            w: int, h: int, x: int, y: int,
+            text: str,
+            font: Union[str, pygame.font.Font] = None,
+            text_color: Tuple[int, int, int] = (0, 0, 0),
+            background_color: Tuple[int, int, int] = (255, 255, 255),
+            hover_color: Tuple[int, int, int] = (255, 255, 255),
+            pressed_color: Tuple[int, int, int] = (255, 255, 255),
+            on_press: Callable = None,
+            continue_press = -1,
+            cursor_change: bool = False):
+        BaseComponent.__init__(self, w, h, x, y)
+
+        # Initialize DEFAULT_FONT after pygame initialized.
+        if TextButton.DEFAULT_FONT is None:
+            TextButton.DEFAULT_FONT = pygame.font.SysFont('simsun', 16)
+
+        self.text = text
+        self.font = font if font is not None else TextButton.DEFAULT_FONT
+        self.text_color = text_color
+        self.text_image = self.font.render(text, True, text_color)
+        self.background_color = background_color
+        self.hover_color = hover_color
+        self.pressed_color = pressed_color
+
+        self.smooth_color = SmoothColor(0.06, self.background_color)
+
+        self.on_press = getCallable(on_press)
+
+        # if press time larger than continue_press_thresh
+        # notify obseervers every frame
+        self.continue_press_thresh = continue_press
+        self.continue_press_time = 0
+        self.pressed = False
+
+        self.cursor_change = cursor_change
+
+    def update(self, events=None) -> None:
+        super().update(events)
+
+        if self.pressed:
+            self.smooth_color.setColor(self.pressed_color)
+        elif self.active:
+            self.smooth_color.setColor(self.hover_color)
+        else:
+            self.smooth_color.setColor(self.background_color)
+
+    def draw(self, surface: pg_Surface) -> None:
+        w, h, x, y = self.getRect()
+        pygame.draw.rect(surface, self.smooth_color.getCurrentColor(), (x, y, w, h))
+
+        text_x = x + (w - self.text_image.get_width()) // 2
+        text_y = y + (h - self.text_image.get_height()) // 2
+        surface.blit(self.text_image, (text_x, text_y))
