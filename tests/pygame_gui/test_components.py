cb1c5550791fcc25e7294a9541aec10cd38f8188
xjtu_zsy <2571263862@qq.com>
2024-11-30 20:39:13 +0800

    modified:   tests/pygame_gui/test_components.py

--- tests/pygame_gui/test_components.py
+++ tests/pygame_gui/test_components.py
@@ -1,14 +1,14 @@
 import unittest
 from unittest.mock import MagicMock
 import pygame
-
 import sys
 import os
 
 print(os.path.dirname(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
-from src.pygame_gui.components.root import Root
+from src.pygame_gui.components.root import *
+from src.pygame_gui.components.base import *
 
 class TestRoot(unittest.TestCase):
     def setUp(self):
@@ -17,18 +17,22 @@ class TestRoot(unittest.TestCase):
 
     def tearDown(self):
         pygame.quit()
-
-    def test_root_initialization(self):
+        
+    def test_init(self):
         root = Root()
-        self.assertEqual(root.screen, self.screen)
-        self.assertIsInstance(root.redraw_tree, _RedrawNode)
-        self.assertFalse(root.redraw_tree.needs_redraw)
+        self.assertIsInstance(root, Root)
 
-    def test_redraw_method(self):
-        root = Root()
-        with self.assertLogs(logger, level='WARNING') as log:
-            root.redraw()
-        self.assertIn("Root can not be redrawn directly.", log.output[0])
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+
+    def tearDown(self):
+        pygame.quit()
+    def test_init(self):
+        base = Base(0,0,0,0)
+        self.assertIsInstance(base, Base)
+    
 
 if __name__ == "__main__":
     unittest.main()
