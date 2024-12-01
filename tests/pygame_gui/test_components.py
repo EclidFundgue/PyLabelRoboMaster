fe13000300162df3d40f9a441f5aaefff61f4c74
xjtu_zsy <2571263862@qq.com>
2024-12-01 15:36:57 +0800

    modified:   tests/pygame_gui/test_components.py

--- tests/pygame_gui/test_components.py
+++ tests/pygame_gui/test_components.py
@@ -1,4 +1,3 @@
-
 import unittest
 from unittest.mock import MagicMock
 
@@ -6,8 +5,9 @@ import pygame
 
 from src.pygame_gui.components.base import Base
 from src.pygame_gui.components.root import Root
-        
-class TestRoot(unittest.TestCase):
+
+
+class TestBase(unittest.TestCase):
     def setUp(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
@@ -18,6 +18,16 @@ class TestRoot(unittest.TestCase):
     def test_init(self):
         base = Base(0,0,0,0)
         self.assertIsInstance(base, Base)
+        
+class TestRoot(unittest.TestCase):
+    def setUp(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+
+    def tearDown(self):
+        pygame.quit()
+        
+    def test_init(self):
         root = Root()
         self.assertIsInstance(root, Root)
     
