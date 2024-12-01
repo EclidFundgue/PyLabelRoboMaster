e7507a4faa376b725775a4d863df210dd3e73c9f
xjtu_zsy <2571263862@qq.com>
2024-12-01 15:30:56 +0800

    modified:   tests/pygame_gui/test_components.py

--- tests/pygame_gui/test_components.py
+++ tests/pygame_gui/test_components.py
@@ -1,17 +1,12 @@
-import os
-import sys
+
 import unittest
 from unittest.mock import MagicMock
 
 import pygame
 
-print(os.path.dirname(__file__))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
-from src.pygame_gui.components.base import *
-from src.pygame_gui.components.root import *
-
-
+from src.pygame_gui.components.base import Base
+from src.pygame_gui.components.root import Root
+        
 class TestRoot(unittest.TestCase):
     def setUp(self):
         pygame.init()
@@ -21,19 +16,10 @@ class TestRoot(unittest.TestCase):
         pygame.quit()
         
     def test_init(self):
-        root = Root()
-        self.assertIsInstance(root, Root)
-
-class TestBase(unittest.TestCase):
-    def setUp(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
-
-    def tearDown(self):
-        pygame.quit()
-    def test_init(self):
         base = Base(0,0,0,0)
         self.assertIsInstance(base, Base)
+        root = Root()
+        self.assertIsInstance(root, Root)
     
 
 if __name__ == "__main__":
